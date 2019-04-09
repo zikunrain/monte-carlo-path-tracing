3dd302ff7ddf38ed1b265113495dd1e7214933ea
Zikun Deng <zikun.rain@gmail.com>
2019-04-09 16:52:01 +0800

    final

--- IModel.py
+++ IModel.py
@@ -5,9 +5,9 @@ from IVec3 import Vec3
 class Model(object):
 
     patterns = {
-        'get_value': re.compile(r'\w+ (.+)\n'),
-        'get_list': re.compile(r'\w+ ([0-9\.-]+) ([0-9\.-]+) ([0-9\.-]+)\n'),
-        'get_vertice_info': re.compile(r'(\d+)/\d*/(\d+)')
+        're_get_value': re.compile(r'\w+ (.+)\n'),
+        're_get_list': re.compile(r'\w+ ([0-9\.-]+) ([0-9\.-]+) ([0-9\.-]+)\n'),
+        're_get_vertice_info': re.compile(r'(\d+)/\d*/(\d+)')
     }
         
     
@@ -16,26 +16,23 @@ class Model(object):
         self.materials = []
         self.faces = []
 
-        self.processFiles()
+        self.processMaterials()
+        self.processObj()
 
         print('Model %s loaded: %d faces, %d materials' % (name, len(self.faces), len(self.materials)))
 
-    def processFiles(self):
-        self.processMTLFile()
-        self.processOBJFile()
-
     # mtl 文件处理
-    def processMTLFile(self):
+    def processMaterials(self):
         f = open(self.filenames[1], 'r')
 
         materials = []
         for line in f.readlines():
             if line[0:6] == 'newmtl':
                 materials.append(Material())
-                value = self.patterns['get_value'].match(line).group(1)
+                value = self.patterns['re_get_value'].match(line).group(1)
                 materials[len(materials) - 1].name = value
             elif line[0] == 'K':
-                search_obj = self.patterns['get_list'].match(line)
+                search_obj = self.patterns['re_get_list'].match(line)
                 light = [float(search_obj.group(1)), float(search_obj.group(2)), float(search_obj.group(3))]
 
                 if line[1] == 'a':
@@ -45,7 +42,7 @@ class Model(object):
                 elif line[1] == 's':
                     materials[len(materials) - 1].Ks = light
             elif line[0] == 'N':
-                search_obj = self.patterns['get_value'].match(line)
+                search_obj = self.patterns['re_get_value'].match(line)
                 value = float(search_obj.group(1))
 
                 if line[1] == 'i':
@@ -53,13 +50,11 @@ class Model(object):
                 elif line[1] == 's':
                     materials[len(materials) - 1].Ns = value
 
-        # for m in materials:                 
-        #     m.print()
         self.materials = materials
         f.close()
 
     # obj 文件处理
-    def processOBJFile(self):
+    def processObj(self):
         f = open(self.filenames[0], 'r')
         faces = []
         vertices = []
@@ -68,16 +63,16 @@ class Model(object):
 
         for line in f.readlines():
             if line[0:2] == 'v ':
-                search_obj = self.patterns['get_list'].match(line)
+                search_obj = self.patterns['re_get_list'].match(line)
                 vertice_pos = [float(search_obj.group(1)), float(search_obj.group(2)), float(search_obj.group(3))]
                 vertices.append(vertice_pos)
             elif line[0:2] == 'vn':
-                search_obj = self.patterns['get_list'].match(line)
+                search_obj = self.patterns['re_get_list'].match(line)
                 normal = [float(search_obj.group(1)), float(search_obj.group(2)), float(search_obj.group(3))]
                 normals.append(normal)
             elif line[0:6] == 'usemtl':
-                name = self.patterns['get_value'].match(line).group(1)
-                curr_mtl_id = self.getMTLIdByName(name)
+                name = self.patterns['re_get_value'].match(line).group(1)
+                curr_mtl_id = self.getMaterialIdByName(name)
                 if curr_mtl_id == -1:
                     print('Material Not Found: ', name)           
             elif line[0:2] == 'f ':
@@ -86,7 +81,7 @@ class Model(object):
                 face_normal = None
 
                 for s in face_vertices_strlist:
-                    search_obj = self.patterns['get_vertice_info'].match(s)
+                    search_obj = self.patterns['re_get_vertice_info'].match(s)
                     vertice_id = int(search_obj.group(1)) - 1
                     face_vertices.append(vertices[vertice_id])
 
@@ -95,18 +90,17 @@ class Model(object):
                         face_normal = normals[normal_id]
 
                 # 分割多边形
-                vertices_count = len(face_vertices)
-                for i in range(vertices_count):
-                    if i > 0 and i < vertices_count - 1:
+                verticesCount = len(face_vertices)
+                for i in range(verticesCount):
+                    if i > 0 and i < verticesCount - 1:
                         triangle = [face_vertices[0], face_vertices[i], face_vertices[i + 1]]
                         face = Face(triangle, face_normal, self.materials[curr_mtl_id])
                         faces.append(face)
 
         self.faces = faces
-        faces[len(faces) - 1].print()
         f.close()
 
-    def getMTLIdByName(self, name):
+    def getMaterialIdByName(self, name):
         for id, mtl in enumerate(self.materials):
             if mtl.name == name:
                 return id
@@ -168,7 +162,7 @@ class Face(object):
         
 
     def print(self):
-        print('-------------- Face --------------')
+        print('-- Face --/ debug')
         print('Vertices: ', self.vertices)
         print('Normal: ', self.normal)
         print('Material: ', self.material.name)
@@ -185,7 +179,7 @@ class Material(object):
         self.Ni = None
 
     def print(self):
-        print('-------------- Material --------------')
+        print('-- Material --/ debug')
         print('Name: ' + self.name)
         print('Ka: ', self.Ka)
         print('Kd: ', self.Kd)
